9b655006fc75d4b51964e74f7722a94944f25cba
Ruslan Himadieiev <113462988+Himadieiev@users.noreply.github.com>
2023-11-22 16:14:14 +0200

    added projects

--- src/assets/index.js
+++ src/assets/index.js
@@ -6,6 +6,13 @@ import instagram from "./instagram.png";
 import facebook from "./facebook.png";
 import twitter from "./twitter.png";
 import skills from "./skills-image.png";
+import project1 from "./project-1.jpeg";
+import project2 from "./project-2.jpeg";
+import project3 from "./project-3.jpeg";
+import project4 from "./project-4.jpeg";
+import project5 from "./project-5.jpeg";
+import project6 from "./project-6.jpeg";
+import project7 from "./project-7.jpeg";
 
 export {
   closeIcon,
@@ -16,4 +23,11 @@ export {
   facebook,
   twitter,
   skills,
+  project1,
+  project2,
+  project3,
+  project4,
+  project5,
+  project6,
+  project7,
 };
